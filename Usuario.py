0dfe2df3c933a4638be0ce0b0e2f0f0f23dc4b2c
davidchica1-creator <david.chica1@udea.edu.co>
2026-05-03 16:05:21 -0500

    Clase usuario

---
+++ Usuario.py
@@ -0,0 +1,16 @@
+class Usuario:
+    
+    def __init__(self,nombre:str, usuario:int, contrasena:str, tipo_usuario:int ):
+        self.__nombre = nombre
+        self.__usuario = usuario
+        self.__contrasena = contrasena
+        self.__tipo_usuario = tipo_usuario
+
+    def get_nombre(self) -> str:
+
+        return self.__nombre
+    
+    def get_tipo_usuario(self) -> int:
+
+        return self.__tipo_usuario
+    

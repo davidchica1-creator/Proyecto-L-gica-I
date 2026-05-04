daab4a3ddebce29eb6d4a89ab12ce56e9b7ba81c
davidortiz3 <david.ortiz3@udea.edu.co>
2026-05-04 17:03:31 -0500

    nuevo

--- SistemaCine.py
+++ SistemaCine.py
@@ -5,9 +5,8 @@ class sistemaCine:
         opcion:int
         print("Bienvenidos a ¿Qué hay para ver?")
         opcion=int(input("Ingresa una de las opciones:\n1.Ingresar.\n2.Salir\n"))
-        
+
         
 
-quehay:sistemaCine
-quehay=sistemaCine()
-quehay.login()
+        
+

3e80fd873f8942d043a6eb04256b3e048f7e40ad
davidortiz3 <david.ortiz3@udea.edu.co>
2026-05-04 16:43:09 -0500

    pipi

---
+++ SistemaCine.py
@@ -0,0 +1,13 @@
+from Usuario import*
+class sistemaCine:
+
+    def login(self)->None:
+        opcion:int
+        print("Bienvenidos a ¿Qué hay para ver?")
+        opcion=int(input("Ingresa una de las opciones:\n1.Ingresar.\n2.Salir\n"))
+        
+        
+
+quehay:sistemaCine
+quehay=sistemaCine()
+quehay.login()

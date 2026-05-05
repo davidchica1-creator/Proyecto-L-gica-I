6c5977e3e46533f3154189cecdccf90424c0ed4f
davidchica1-creator <david.chica1@udea.edu.co>
2026-05-04 21:57:29 -0500

    correcion

--- funciones_utiles.py
+++ funciones_utiles.py
@@ -14,32 +14,26 @@ def solicitar_dato(mensaje, tipo_esperado, min_val=None, max_val=None):
     while True:
         entrada = input(mensaje).strip()
         
-        # 1. Validar que no esté vacío
         if not entrada:
             print("Error: El campo no puede estar vacío.")
             continue
             
-        # 2. Validación de TEXTO
         if tipo_esperado == 'texto':
             if entrada.isdigit():
                 print("Error: El texto no puede ser solo números.")
                 continue
             
-            # Verificamos que el texto tenga al menos una letra del abecedario.
-            # Así evitamos que ingresen solo símbolos como "---" o "!!!"
             tiene_letra = False
             for caracter in entrada:
                 if caracter.isalpha():
                     tiene_letra = True
-                    break # Si encontramos al menos una letra, dejamos de buscar
-            
+                    break 
             if not tiene_letra:
                 print("Error: El texto debe contener letras válidas, no solo símbolos.")
                 continue
                 
-            return entrada # Si pasa todas las pruebas, retorna el texto
+            return entrada 
             
-        # 3. Validación de NÚMERO
         elif tipo_esperado == 'numero':
             try:
                 valor = int(entrada)
@@ -54,11 +48,9 @@ def solicitar_dato(mensaje, tipo_esperado, min_val=None, max_val=None):
                 
             except ValueError:
                 print("Error: Debes ingresar un número entero válido.")
-                continue # Obliga a reiniciar el ciclo while inmediatamente
-        
-        # 4. Seguro contra errores de programación
+                continue 
+
         else:
-            # Si el programador manda un tipo_esperado diferente a 'texto' o 'numero'
             print(f"Error interno: El tipo esperado '{tipo_esperado}' no existe.")
             return None
 
